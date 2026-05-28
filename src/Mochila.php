c9bd8a64872414edc35e7fd2a21de618ec52ac30
Endika_L <endik200302@gmail.com>
2026-05-28 10:42:38 +0200

    Initial commit

---
+++ src/Mochila.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Deg540\MochilaAventura;
+
+class Mochila
+{
+
+}
---
+++ tests/MochilaTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Deg540\MochilaAventura\Test;
+
+use PHPUnit\Framework\TestCase;
+use Deg540\MochilaAventura\Mochila;
+class MochilaTest extends TestCase
+{
+
+}
