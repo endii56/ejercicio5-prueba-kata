37f8ba7c3d20f1230de754cc3b157b687a4ade7e
Endika_L <endik200302@gmail.com>
2026-05-28 11:37:07 +0200

    [ROJO] - Test hola mundo

--- tests/MochilaTest.php
+++ tests/MochilaTest.php
@@ -6,5 +6,15 @@ use PHPUnit\Framework\TestCase;
 use Deg540\MochilaAventura\Mochila;
 class MochilaTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function testHolaMundo():void
+    {
+        $mochila = new Mochila();
 
+        $respuesta = $mochila->holaMundo();
+
+        $this->assertEquals("Hola mundo!", $respuesta);
+    }
 }
