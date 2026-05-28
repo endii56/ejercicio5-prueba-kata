e060f39db368e1bfa056658e2882eb0dc392e621
Endika_L <endik200302@gmail.com>
2026-05-28 11:52:27 +0200

    [REFACTOR] - Separar responsabilidades de la funcion principaly sacar constantes

--- src/Mochila.php
+++ src/Mochila.php
@@ -7,19 +7,26 @@ class Mochila
 
     private array $mochila;
 
+    private const string ACCION_GUARDAR = "guardar";
     public function __construct()
     {
         $this->mochila = [];
     }
 
     public function ejecutar(string $accion):string{
-        [$accion, $objeto, $cantidad] = explode(" ", $accion);
-        if($accion == "guardar"){
-            $this->mochila[$objeto] = $cantidad;
-        }
-        if(empty($this->mochila)){
-            return "";
+        [$accion, $objeto, $cantidad] = $this->obtenerParametros($accion);
+        if($accion == self::ACCION_GUARDAR){
+            $this->guardar($objeto, $cantidad);
         }
         return implode(" ", $this->mochila);
+    }
+
+    public function obtenerParametros(string $accion):array{
+        $parametros = explode(" ", $accion);
+        return [$parametros[0],$parametros[1],$parametros[2]];
+    }
+
+    public function guardar(string $objeto, string $cantidad):void{
+        $this->mochila[$objeto] = $cantidad;
     }
 }
