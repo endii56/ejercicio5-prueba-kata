549cfeace8e1be7316cb477d815c7e26c4feb5be
Endika_L <endik200302@gmail.com>
2026-05-28 11:40:58 +0200

    [VERDE] - Test inicializar una mochila vacia

--- src/Mochila.php
+++ src/Mochila.php
@@ -5,8 +5,22 @@ namespace Deg540\MochilaAventura;
 class Mochila
 {
 
+    private array $mochila;
+
+    public function __construct()
+    {
+        $this->mochila = [];
+    }
+
     public function holaMundo():string
     {
         return "Hola mundo!";
+    }
+
+    public function ejecutar():string{
+        if(empty($this->mochila)){
+            return "";
+        }
+        return "la mochila tiene objetos";
     }
 }
